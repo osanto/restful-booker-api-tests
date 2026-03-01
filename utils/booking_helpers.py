0dce9e1345fad7236dbe3446a449a7174073797f
Olha Santo <olha.santo@gmail.com>
2026-03-01 15:24:19 -0600

    Add type hints to booking_helpers.py

--- utils/booking_helpers.py
+++ utils/booking_helpers.py
@@ -3,7 +3,8 @@ from config import Logging
 logger = Logging.setup_logging()
 
 
-def search_created_booking_in_bookings(booking_ids, current_booking_id):
+def search_created_booking_in_bookings(booking_ids: list[dict], current_booking_id: int) -> list[int]:
+    """Search for a booking ID in a list of booking dictionaries. Returns list of matching IDs."""
     logger.info(f"Booking ID we are trying to find: {current_booking_id}")
 
     matching_bookings = [booking['bookingid'] for booking in booking_ids if booking['bookingid'] == current_booking_id]
